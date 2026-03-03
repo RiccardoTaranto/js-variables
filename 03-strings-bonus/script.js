af1f601a21d5947445b17dc734bedb3942158065
Riccardo <riccardo.taranto@icloud.com>
2026-03-03 12:38:31 +0100

    add city index, substring and replace methods

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -3,20 +3,20 @@ const cityName = 'City'; // NON MODIFICARE QUESTA RIGA
 const shoppingList = 'mele, banane, arance, uva'; // NON MODIFICARE QUESTA RIGA
 const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 
-// 1. Cerca un carattere in una stringa
+// 1. Cerca un carattere in una stringa ✅
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
+const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
-// Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
+// Stampa la sottostringa "York" dalla variabile city ✅
+const citySubstring = city.substring(cityIndex);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
-// Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+// Sostituisci 'York' con 'Delhi' ✅
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
